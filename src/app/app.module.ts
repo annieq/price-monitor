59847a3eee2b3afbcdd47875699ec9e8e4b89866
annieq <szoanna27@gmail.com>
2021-03-29 16:27:40 +0200

    cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+const APP_MODULES = [
+  // TODO define here custom modules
+];
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    // ...APP_MODULES
   ],
   providers: [],
   bootstrap: [AppComponent]
